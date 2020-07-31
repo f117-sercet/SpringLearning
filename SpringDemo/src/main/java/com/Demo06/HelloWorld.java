0623e3c2be829878ce182888acb7cd83b7aa960d
f117-serect <15159885771@163.com>
2020-07-31 15:58:53 +0800

    SpringDemo

---
+++ SpringDemo/src/main/java/com/Demo06/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.Demo06;
+
+/**
+ * 如果没有提供一个值，就无法创建实例，可以通过value来指定值
+ * @author  dsc
+ */
+public class HelloWorld {
+    String messgae;
+
+    public HelloWorld(String messgae) {
+        this.messgae = messgae;
+    }
+}
