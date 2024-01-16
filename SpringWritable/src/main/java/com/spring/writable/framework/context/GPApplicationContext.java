b7261a7a66588f7ed7308bda13bed4fcb22a84a3
sowrd <15159885771@163.com>
2024-01-16 16:01:38 +0800

    DI

--- SpringWritable/src/main/java/com/spring/writable/framework/context/GPApplicationContext.java
+++ SpringWritable/src/main/java/com/spring/writable/framework/context/GPApplicationContext.java
@@ -1,7 +1,16 @@
 package com.spring.writable.framework.context;
 
+import com.spring.writable.framework.beans.GPBeanDefinition;
+import com.spring.writable.framework.beans.GPBeanWrapper;
+import com.spring.writable.framework.context.support.GPAbstractApplicationContext;
+import com.spring.writable.framework.context.support.GPBeanDefinitionReader;
 import com.spring.writable.framework.context.support.GPDefaultListableBeanFactory;
 import com.spring.writable.framework.core.GPBeanFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Description： TODO
@@ -12,7 +21,22 @@ import com.spring.writable.framework.core.GPBeanFactory;
 public class GPApplicationContext extends GPDefaultListableBeanFactory implements GPBeanFactory {
 
     private String[] configLocations;
-    private GP
+    private GPBeanDefinitionReader reader;
+    
+    // 单例的IOC容器缓存
+    private Map<String, Object> factoryBeanObjectCache = new ConcurrentHashMap<String,Object>();
+    //通用的IOC模块
+    private Map<String, GPBeanWrapper> factoryBeanInstanceCache = new ConcurrentHashMap<String,GPBeanWrapper>();
+    
+    public GPApplicationContext(String... configLocations) {
+        this.configLocations = configLocations;
+        try{
+            refresh();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
     @Override
     public Object getBean(String beanName) {
         return null;
@@ -25,6 +49,21 @@ public class GPApplicationContext extends GPDefaultListableBeanFactory implement
 
     @Override
     public void refresh() throws Exception {
-        super.refresh();
+        //1.定位，定位配置文件
+        reader  = new GPBeanDefinitionReader(this.configLocations);
+        
+        //2.加载配置文件
+        List<GPBeanDefinition> beanDefinitions = reader.loadBeanDefinitions();
+        
+        //3.注册，把配置信息放到容器里
+        doRegisterBeanDefinition(beanDefinitions);
+        //4.把不是延时加载的       类提前初始化
+        doAutowrited();
+    }
+
+    private void doAutowrited() {
+    }
+
+    private void doRegisterBeanDefinition(List<GPBeanDefinition> beanDefinitions) {
     }
 }
